d914d0fca35f516e2ffcff92fee0bc3928f08560
juechen-zzz <240553516@qq.com>
2020-01-09 09:34:27 +0800

    first commit

---
+++ C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++
+//
+//  Created by 倪浩鹏 on 2020/1/9.
+//  Copyright © 2020 倪浩鹏. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
