24670c06ba61d59ec7d19826bcd32c21c06c9d84
juechen-zzz <240553516@qq.com>
2020-01-16 21:23:09 +0800

    add in 2020-1-16

---
+++ C_project/C_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C_project
+//
+//  Created by 倪浩鹏 on 2020/1/16.
+//  Copyright © 2020 nihaopeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,17 +1,16 @@
 //
-//  main.cpp
+//  test.cpp
 //  C++
 //
-//  Created by 倪浩鹏 on 2020/1/9.
 //  Copyright © 2020 nihaopeng. All rights reserved.
 //
 
 // 简单写一个冒泡排序
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+int main() {
     // insert code here...
-    int a[10] = {3, 1, 4, 5, 2};
+    int a[10] = {3, 5, 657, 17, 5};
     for (int i = 1; i <= 4; i++) {
         for (int j = 0; j < 5 - i; j++) {
             if (a[j] > a[j+1]) {
