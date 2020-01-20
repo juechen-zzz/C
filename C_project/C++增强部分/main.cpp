8b2d283768c4dd3c9bea5360adac022efa264671
juechen-zzz <240553516@qq.com>
2020-01-20 13:40:55 +0800

    add some in 2020-1-20

--- C_project/C++增强部分/main.cpp
+++ C_project/C++增强部分/main.cpp
@@ -19,19 +19,23 @@ void test(){
     cout << "size: " << sizeof(flag) << endl;       // 大小为1个字节
 }
 
+void test2(){
+    int a = 10;
+    int b = 20;
+    int c = 0;
+    
+    c = a > b ? a : b;          // 三目运算符(c语言中的写法，C++中可以作为左值，返回的是a的引用)
+    cout << "c = " << c << endl;
+    
+    // *(a < b ? &a : &b) = 50;    // 返回的变成了a或者b的地址，然后赋值(c语言)
+    (a < b ? a : b) = 50;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+}
+
 int main(int argc, const char * argv[]) {
     cout << "Hello, World!" << endl;
     test();
+    test2();
     return 0;
 }
-
-
-//// C语言的写法
-//#include <stdio.h>
-//
-//int main(){
-//    int a = 10;
-//    int b = 20;
-//    printf("%d, %d\n", a, b);
-//    return 0;
-//}
