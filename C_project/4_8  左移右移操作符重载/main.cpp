9e3ee5ae226269b327a843311dfbb15f60b9fa32
juechen-zzz <240553516@qq.com>
2020-02-04 23:45:04 +0800

    add 4_6 - 4_8

---
+++ C_project/4_8  左移右移操作符重载/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  4_8  左移右移操作符重载
+//
+//  Created by 倪浩鹏 on 2020/2/4.
+//  Copyright © 2020 nihaopeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
